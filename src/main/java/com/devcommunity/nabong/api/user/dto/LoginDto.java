d6f36f28bbadac2f15e3b1846842b01be545efcf
nabong99 <skgusslarj@naver.com>
2022-04-08 20:12:36 +0900

    Security설정, Data설정2

---
+++ src/main/java/com/devcommunity/nabong/api/user/dto/LoginDto.java
@@ -0,0 +1,2 @@
+package com.devcommunity.nabong.api.user.dto;public class LoginDto {
+}
---
+++ src/main/java/com/devcommunity/nabong/api/user/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package com.devcommunity.nabong.api.user.vo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
